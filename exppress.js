9121b9b15f9ebe04502ae79382d9728714fd8968
Ellen Fawkes <fawkes@ttc.cz>
2017-12-08 14:54:19 +0100

    Removed wtf file

--- exppress.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-	        console.dir(req);
-		        res.send('Hello World!')
-})
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
